03ddf2247e489edf10e755ea23bc74c27f298b67
Pedro Maia <pedrocmaia2105@gmail.com>
2024-01-22 18:17:25 -0300

    minor fixes

--- API/REST/urls.py
+++ API/REST/urls.py
@@ -2,10 +2,14 @@ from django.contrib import admin
 from django.urls import path
 from REST.views import listaTasks
 from REST.views import criaTask
+from REST.views import task 
+from REST.views import delTask
 
 
 urlpatterns = [
     path('lista',listaTasks),
-    path('task',criaTask)
+    path('task',criaTask),
+    path('delete',delTask),
+    path('<int:pk>',task)
 ]
 
--- API/REST/views.py
+++ API/REST/views.py
@@ -3,7 +3,7 @@ from .models import Task
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.http import JsonResponse
-from serializer import Task_serializer
+from .serializer import Task_serializer
 
 
 def valida_task(receivedTask):
@@ -26,15 +26,40 @@ def criaTask(request):
     task = Task_serializer(data=request.data)
     if task.is_valid():
         task.save()
+        return Response({'message': 'Task criada com sucesso'})
     else:
-        return Response({'message': 'Erro ao criar task'})
+        return Response({'message': task.errors})
     
 @api_view(['DELETE'])
 def delTask(request):
     try:
         task = Task.objects.get(id=request.data['id'])
         task.delete()
+        return Response({'message': 'Task deletada com sucesso'})
     except Exception as erro:
         return Response({'message': 'Erro ao deletar task/n'+ str(erro)})
     
 
+@api_view(['GET','POST','DELETE'])
+def task(request,primary_key):
+    if request.method == 'GET':
+        try:
+            task = Task.objects.get(primary_key = primary_key)
+            taskSerializada = Task_serializer(task)
+            return Response(taskSerializada.data)
+        except:
+            return Response({'message': 'Nenhuma task com esse id foi encontrada'})
+    elif request.method == 'POST':
+        try:
+            task = Task_serializer(data=request.data)
+            if task.is_valid():
+                task.save()
+            else: raise Exception('A task passada possui valores inválidos')
+        except:
+            return Response()
+    elif request.method == 'DELETE': 
+        try:
+            task = Task.objects.get(id=primary_key)
+            task.delete()
+        except:
+            return Response({'message': 'Erro ao deletar task'})
